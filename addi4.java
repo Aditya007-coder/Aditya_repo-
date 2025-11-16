c40a56b641bb74008d28882d9542601864515489
Aditya007-coder <sonawaneaditya424@gmail.com>
2025-11-16 13:25:24 +0000

    This is 5 java code

---
+++ addi4.java
@@ -0,0 +1,9 @@
+public class Largest {
+    public static void main(String[] args) {
+        int a = 10, b = 15;
+        if(a > b)
+            System.out.println(a + " is largest");
+        else
+            System.out.println(b + " is largest");
+    }
+}
